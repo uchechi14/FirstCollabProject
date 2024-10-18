993103431fad48ae0ce90b0e81a56944d5fc87f4
uchechi14 <uchechinnorom9@gmail.com>
2024-10-18 15:45:57 +0100

    updateCommit

--- src/components/UploadInput.jsx
+++ src/components/UploadInput.jsx
@@ -1,7 +1,7 @@
-const UploadInput = () => {
+const UploadInput = ({name,handleChange}) => {
   return (
     <div className="w-[100%]">
-        <input type="text" className="w-full border rounded-[4px] py-2 outline-none pl-[10px]" />
+        <input name={name} onChange={handleChange} type="text" className="w-full border rounded-[4px] py-2 outline-none pl-[10px]" />
         
     </div>
   )
