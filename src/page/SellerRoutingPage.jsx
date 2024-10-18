54fd9b6ca2829578d53b42a2c937d08a1c0e2077
DeborahIwuanyanwu <deborahiwuanyanwu@gmail.com>
2024-10-18 11:24:42 +0100

    updates

--- src/page/SellerRoutingPage.jsx
+++ src/page/SellerRoutingPage.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from 'react-router-dom'
 // import SellerHeader from '../components/SellerPageComponents/SellerHeader'
 // import LandingPage from './LandingPage'
 import SellerPage from './SellerPage'
+import UploadProducts from './uploads/UploadProducts'
 
 const SellerRoutingPage = () => {
   return (
    <Routes>
         <Route path="dashboard/" element={<SellerPage />} />
+        <Route path="/upload_page" element={<UploadProducts />} />
  
 
 
